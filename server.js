1e69a83e50f37f252aaf1de7f8ec1ce28b8dbbb8
bjb568 <brianjblair@icloud.com>
2015-10-26 19:10:31 -0400

    Use SSL

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
-var http = require('http'),
+var http = require('https'),
 	fs = require('fs');
-http.createServer(function(req, res) {
+
+var constants = require('constants'),
+	SSL_ONLY_TLS_1_2 = constants.SSL_OP_NO_TLSv1_1|constants.SSL_OP_NO_TLSv1|constants.SSL_OP_NO_SSLv3|constants.SSL_OP_NO_SSLv2;
+
+http.createServer({
+	key: fs.readFileSync('../Secret/devdoodle.net.key'),
+	cert: fs.readFileSync('../Secret/devdoodle.net.crt'),
+	ca: [fs.readFileSync('../Secret/devdoodle.net-geotrust.crt')],
+	ecdhCurve: 'secp384r1',
+	ciphers: [
+		'ECDHE-ECDSA-AES256-GCM-SHA384',
+		'ECDHE-RSA-AES256-GCM-SHA384',
+		'ECDHE-ECDSA-AES128-GCM-SHA256',
+		'ECDHE-RSA-AES128-GCM-SHA256',
+		'ECDHE-ECDSA-AES256-SHA',
+		'ECDHE-RSA-AES256-SHA'
+	].join(':'),
+	honorCipherOrder: true,
+	secureOptions: SSL_ONLY_TLS_1_2
+}, function(req, res) {
 	console.log(req.url);
 	if (req.url.indexOf('.css') != -1) {
 		res.writeHead(200, {
